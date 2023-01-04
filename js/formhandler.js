e24afe5a2214d7140bbda826f4e0dda46ba94973
Gianmarco <gianmfed@gmail.com>
2023-01-04 12:40:19 +0100

    changing style of the send button after sending

--- js/formhandler.js
+++ js/formhandler.js
@@ -1,15 +1,10 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-  // Validate the form data here
-  // Call event.preventDefault() if the form is not valid
-  console.log("in function")
   // Prevent the default form submission action
   event.preventDefault()
   // Get the form data
   const formData = new FormData(event.target);
-  var name = formData.get('message');
-  console.log(name)
-//});
-  
+  var message = formData.get('message');
+  console.log(message)  
 
   // Send the form data to the server via AJAX
   const xhr = new XMLHttpRequest();
@@ -21,13 +16,19 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     if (xhr.status === 200) {
         // The form was submitted successfully
         // Update the page or display a success message
-        // ...
+        const button = document.getElementById('submitButton');
+        console.log(button)
+        button.innerHTML = 'Thankyou!';
+        button.style.backgroundColor = 'green'
+        button.style.borderColor = 'Chartreuse'
+        ###########################################################
+        button.classList.remove('btn');
+        button.classList.add('btn-primary');
         console.log(xhr.response);
         console.log("form submitted")
       } else {
         // There was an error submitting the form
         // Display an error message or update the page
-        // ...
         console.log("error")
       }
     }
