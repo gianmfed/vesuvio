a3ed432745dba1dda49d37f024132a601ab54794
Gianmarco <gianmfed@gmail.com>
2023-01-23 11:42:31 +0100

    Cambiare colore nero di bg-custom

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,8 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Enable popovers
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 });
